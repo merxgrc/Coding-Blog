e8d35d6fee4a29b7905b083b27416d81a285dba4
merxgrc <yeetgawdmcneckass@gmail.com>
2025-02-24 12:08:14 -0800

    Setup the home page andJournal, Projects, Tutorials pages were created.

---
+++ src/app/projects/page.js
@@ -0,0 +1,8 @@
+export default function Projects() {
+    return (
+        <main className="min-h-screen flex flex-col items-center p-6 bg-gray-800">
+            <h1 className="text-3xl">Project Demos</h1>
+            <p className="mt-2">Showcase of coding projects.</p>
+        </main>
+    )
+}
---
+++ src/app/tutorials/page.js
@@ -0,0 +1,8 @@
+export default function Tutorials(){
+    return (
+        <main className="min-h-screen flex flex-col items-center p-6 bg-gray-800">
+            <h1 className="text-3xl">Tutorials</h1>
+            <p className="mt-2">Tutorials coming soon!</p>
+        </main>
+    )
+}
