d2dadc694f3f4dfa0cf70c311e98b8756ed529e1
merxgrc <mgarcia73@mail.sfsu.edu>
2025-07-11 23:31:53 -0700

    deleted the old c++ tab

--- src/app/tutorials/c++/page.js
+++
@@ -1,8 +0,0 @@
-export default function CPlusPlusTutorials() {
-    return (
-        <main className="min-h-screen bg-gray-900 flex flex-col items-center">
-            <h1 className="text-3xl p-6">C++ Tutorials</h1>
-            <a>Tutorials coming soon...</a>
-        </main>
-    )
-}
