c20117a23505ff801ce4e7fd1d9ac70f2ec44d82
merxgrc <mgarcia73@mail.sfsu.edu>
2025-09-23 18:30:09 -0700

    adds white to home

--- src/app/components/HomeButton.js
+++ src/app/components/HomeButton.js
@@ -5,7 +5,7 @@ export default function HomeButton() {
     return (
         
       <div className="fixed top-6 left-10">
-        <Link href="/" className="text-xl font-bold hover:text-blue-400">
+        <Link href="/" className=" text-white text-xl font-bold hover:text-blue-400">
           Marcos Garcia
         </Link>
       </div>
